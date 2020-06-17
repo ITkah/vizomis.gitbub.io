171dd1e1f445b229313687a1ae87809c774e8586
ITkah <ilatkacenko132@gmail.com>
2020-06-17 14:37:00 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -2,3 +2,7 @@ $(".show-about-text").on("click", function(){
     $(".home-text").toggleClass("max");
     $(this).toggleClass("active-show");
 });
+
+$(".heart-box").on("click", function(){
+    $(this).toggleClass("heart-click");
+});
