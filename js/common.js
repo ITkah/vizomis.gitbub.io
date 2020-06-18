aa982dce197bd2bf98e7110a3c7b1f4007ad1df9
ITkah <ilatkacenko132@gmail.com>
2020-06-18 19:10:46 +0300

    last-fix

--- js/common.js
+++ js/common.js
@@ -1,17 +1,61 @@
-$(".show-about-text").on("click", function(){
-    $(".home-text").toggleClass("max");
-    $(this).toggleClass("active-show");
-});
+$(document).ready(function() {
 
-$(".heart-box").on("click", function(){
-    $(this).toggleClass("heart-click");
-});
+    $(".show-about-text").on("click", function(){
+        $(".home-text").toggleClass("max");
+        $(this).toggleClass("active-show");
+    });
 
-$(".header-lang-box").on("click", function(){
-    $(this).toggleClass("active-lang");
-    $(".lang-hide").slideToggle(150);
-});
+    $(".heart-box").on("click", function(){
+        $(this).toggleClass("heart-click");
+    });
 
-$(".menu-desktop").on("click", function(){
-    $(".menu-1").slideToggle(250);
+    $(".header-lang-box").on("click", function(){
+        $(this).toggleClass("active-lang");
+        $(".lang-hide").slideToggle(150);
+    });
+
+    $(".menu-desktop").on("click", function(){
+        $(".menu-1").slideToggle(250);
+    });
+
+    $(".search-call").on("click", function(){
+        $(".header-search-box").toggleClass("active-drop"); 
+        $("html,body").toggleClass("active-body");
+    });
+
+    $(".close-search").on("click", function(){
+        $(".header-search-box").removeClass("active-drop");
+        $("html,body").removeClass("active-body"); 
+    });
+
+    $(".call-nav").on("click", function(){
+        $(".overflow").fadeToggle();
+        $("nav").toggleClass("active-drop"); 
+        $("html,body").toggleClass("active-body");
+    });
+
+    $(".close-menu").on("click", function(){
+        $(".overflow").hide();
+        $("nav").removeClass("active-drop");
+        $("html,body").removeClass("active-body"); 
+    });
+
+    $(".header-search-box input").on("click", function(){
+        $(".result-search-wrap").fadeToggle(150);
+    });
+
+    $(".brend-name, .brend-xl").on("click", function(){
+        $(this).siblings(".brend-hide-wrap").addClass("active-drop");
+    });
+
+    $(".brend-hide-panel").on("click", function(){
+        $(".brend-hide-wrap").removeClass("active-drop");
+    });
+
+    $('.tabs__caption').on('click', 'p:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('.mobile-menu-nav').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
 });
