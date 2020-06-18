c89adbf7d2fe8a838e5c749dd69d74ddd065bc6a
ITkah <ilatkacenko132@gmail.com>
2020-06-18 15:06:56 +0300

    85%

--- js/common.js
+++ js/common.js
@@ -6,3 +6,12 @@ $(".show-about-text").on("click", function(){
 $(".heart-box").on("click", function(){
     $(this).toggleClass("heart-click");
 });
+
+$(".header-lang-box").on("click", function(){
+    $(this).toggleClass("active-lang");
+    $(".lang-hide").slideToggle(150);
+});
+
+$(".menu-desktop").on("click", function(){
+    $(".menu-1").slideToggle(250);
+});
