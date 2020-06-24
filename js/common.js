9f1c8ad1eb37e821e6ab295e5263f1c12590daf1
ITkah <ilatkacenko132@gmail.com>
2020-06-24 10:07:11 +0300

    add 2 page

--- js/common.js
+++ js/common.js
@@ -1,7 +1,51 @@
 $(document).ready(function() {
 
+    $(function() {
+        menu_top = $('header').offset().top; 
+        $(window).scroll(function () {            
+            if ($(window).scrollTop() > menu_top) {  
+                $('header').addClass("fixed-desk");
+            } else {                                 
+                $('header').removeClass("fixed-desk");
+            }
+        });
+    });
+
+    // if($(document).width() > 990 ? true : false){
+    //     $(function() {
+    //         menu_top = $('header').offset().top; 
+    //         $(window).scroll(function () {            
+    //             if ($(window).scrollTop() > menu_top) {  
+    //                 $('header').addClass("fixed-desk");
+    //             } else {                                 
+    //                 $('header').removeClass("fixed-desk");
+    //             }
+    //         });
+    //     });
+    // } else {
+    //     $(function() {
+    //         menu_top = $('.header-container').offset().top; 
+    //         $(window).scroll(function () {            
+    //           if ($(window).scrollTop() > menu_top) {  
+    //             if ($('.header-container').css('position') != 'fixed') { 
+    //                 $('.header-container').css('position','fixed');  
+    //                 $('.header-container').addClass("fixed-mob");
+    //                 $('main').addClass("main-top"); 
+    //             }
+    //           } else {                                 
+    //             if ($('.header-container').css('position') == 'fixed') {  
+    //               $('.header-container').css('position','');
+    //               $('.header-container').removeClass("fixed-mob");
+    //               $('main').removeClass("main-top"); 
+    //             }
+    //           }
+    //         });
+    //     });  
+    // }
+
     $(".show-about-text").on("click", function(){
         $(".home-text").toggleClass("max");
+        $(this).siblings(".description").toggleClass("max");
         $(this).toggleClass("active-show");
     });
 
@@ -11,7 +55,7 @@ $(document).ready(function() {
 
     $(".header-lang-box").on("click", function(){
         $(this).toggleClass("active-lang");
-        $(".lang-hide").slideToggle(150);
+        $(".lang-hide").slideToggle(50);
     });
 
     $(".search-call").on("click", function(){
@@ -28,6 +72,14 @@ $(document).ready(function() {
         $(".overflow").fadeToggle();
         $("nav").toggleClass("active-drop");
         $("html,body").toggleClass("active-body"); 
+    });
+
+    $(".call-filter").on("click", function(){
+        $(".filter").toggleClass("filter-active");
+    });
+
+    $(".close-filter").on("click", function(){
+        $(".filter").removeClass("filter-active");
     });
 
     $(".close-menu").on("click", function(){
@@ -53,5 +105,29 @@ $(document).ready(function() {
           .addClass('active').siblings().removeClass('active')
           .closest('.mobile-menu-nav').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
+
+    $(".data").on("click", function(){
+        $(this).siblings(".hide-arrow").hide();
+        $(this).attr('type','date');
+    });
+
+    $(".time").on("click", function(){
+        $(this).attr('type','time');
+    });
+
+    $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
 
 });
